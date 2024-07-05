48e0a3e39ba69fd01caa5512b3f364cb4dc1f59e
Henrique Custódia <martinshenrique8@gmail.com>
2024-07-04 21:31:10 -0300

    feat(product-mfe): migra código do outro projeto

--- apps/mfes/products-mfe/src/app/app.component.ts
+++ apps/mfes/products-mfe/src/app/app.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ProductsService } from './shared/services/products.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  standalone: true,
-  imports: [RouterModule],
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrl: './app.component.scss'
 })
-export class AppComponent {}
+export class AppComponent {
+  productsService = inject(ProductsService);
+
+  productsService$ = this.productsService.getProducts();
+}
