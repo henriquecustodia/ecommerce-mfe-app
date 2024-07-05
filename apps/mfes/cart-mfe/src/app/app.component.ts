57285626b6d0fe0cba6a8ff44bdf28af71583568
Henrique Custódia <martinshenrique8@gmail.com>
2024-07-04 21:35:54 -0300

    feat(cart-mfe): migra código do outro projeto

--- apps/mfes/cart-mfe/src/app/app.component.ts
+++ apps/mfes/cart-mfe/src/app/app.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CartService } from './shared/services/cart.service';
+import { Cart } from './shared/interfaces/cart.interface';
 
 @Component({
-  standalone: true,
-  imports: [RouterModule],
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrl: './app.component.scss'
 })
-export class AppComponent {}
+export class AppComponent {
+  
+  cartService = inject(CartService);
+
+  cart = signal<Cart | null>(null);
+
+  ngOnInit(): void {
+    this.cartService.getOne(5).subscribe((cart) => {
+      this.cart.set(cart);
+    })  
+  }
+
+}
