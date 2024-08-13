a1231ffecf6a0d0f834cf02ee8f2bb9f12453357
Henrique Custódia <martinshenrique8@gmail.com>
2024-08-13 20:03:19 -0300

    feat(routes): agrupa rotas da home

--- apps/shells/ecommerce-shell/src/app/app.routes.ts
+++ apps/shells/ecommerce-shell/src/app/app.routes.ts
@@ -4,20 +4,25 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    outlet: 'products',
-    loadComponent: () =>
-      loadRemoteModule({
-        remoteEntry: 'http://localhost:4201/remoteEntry.json',
-        exposedModule: './Component',
-      }).then((m) => m.AppComponent),
-  },
-  {
-    path: '',
-    outlet: 'cart',
-    loadComponent: () =>
-      loadRemoteModule({
-        remoteEntry: 'http://localhost:4202/remoteEntry.json',
-        exposedModule: './Component',
-      }).then((m) => m.AppComponent),
+    children: [
+      {
+        path: '',
+        outlet: 'products',
+        loadComponent: () =>
+          loadRemoteModule({
+            remoteEntry: 'http://localhost:4201/remoteEntry.json',
+            exposedModule: './Component',
+          }).then((m) => m.AppComponent),
+      },
+      {
+        path: '',
+        outlet: 'cart',
+        loadComponent: () =>
+          loadRemoteModule({
+            remoteEntry: 'http://localhost:4202/remoteEntry.json',
+            exposedModule: './Component',
+          }).then((m) => m.AppComponent),
+      },
+    ],
   },
 ];
